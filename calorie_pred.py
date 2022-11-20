12ba719fb09c28dfbadc60bff7a758e6899a9d63
Splitxorpio <mailtoarnavpandey@gmail.com>
2022-11-19 23:33:02 -0800

    Submitting Version
    Co-authored-by: Shervin the Prodigy <Shervi28@users.noreply.github.com>

--- calorie_pred.py
+++ calorie_pred.py
@@ -1,18 +1,61 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import mean_squared_error
+from xgboost import XGBRegressor,XGBClassifier
+import joblib
+import catboost as cb
+import joblib
 
-#Make 4 seperate models for each weight category and package them to joblib for backend
-
+params = {
+    'task': 'train', 
+    'boosting': 'gbdt',
+    'objective': 'regression',
+    'num_leaves': 10,
+    'learnnig_rage': 0.05,
+    'metric': {'l2','l1'},
+    'verbose': -1
+}
 df = pd.read_csv('datasets/exercise_dataset.csv')
 
-X = df.loc[:, ['130 lb']]
-y = df.loc[:, ['Activity, Exercise or Sport (1 hour)']]
 
+X = df[['205 lb']]
+y = df[['Activity, Exercise or Sport (1 hour)']]
+
+print(X)
 print(y)
 
-enc = OneHotEncoder(handle_unknown='ignore')
+enc = LabelEncoder()
 
-dum_df = pd.get_dummies(y, columns=["Activity, Exercise or Sport (1 hour)"], prefix=["Type_is"] )
-print(dum_df)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+y_train,y_test = enc.fit_transform(y_train), enc.fit_transform(y_test)
+
+y_train = y_train.ravel()
+train_y = np.array(y_train).astype(int)
+
+train_dataset = cb.Pool(X_train, train_y) 
+test_dataset = cb.Pool(X_test, y_test)
+
+clf = cb.CatBoostRegressor(loss_function='RMSE')
+grid = {'iterations': [100, 150, 200],
+        'learning_rate': [0.03, 0.1],
+        'depth': [2, 4, 6, 8],
+        'l2_leaf_reg': [0.2, 0.5, 1, 3]
+        }
+clf.grid_search(grid, train_dataset)
+
+
+y_pred = clf.predict(X_test)
+
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+score = mean_squared_error(y_test,y_pred)
+print(score)
+
+filename = '205weightModel.sav'
+joblib.dump(clf, filename)
--- main.py
+++ main.py
@@ -1,11 +1,138 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, session
+import firebase_admin
+import pyrebase
+from firebase_admin import credentials, firestore
+import joblib
+import pandas as pd
+days_of_the_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+today_and_next_day = [20, 21, 22, 23, 24, 25, 19]
+cred = {
+    'apiKey': "AIzaSyCO6fGQosvak27-N1mKe9mJBEld8uMlU20",
+    'authDomain': "froggenics.firebaseapp.com",
+    'projectId': "froggenics",
+    'storageBucket': "froggenics.appspot.com",
+    'messagingSenderId': "571796120674",
+    'appId': "1:571796120674:web:4b6a4725097ab51ddddd3b",
+    'measurementId': "G-QLN38H2TS6",
+    'databaseURL': ""
+}
+firebase = pyrebase.initialize_app(cred)
+auth = firebase.auth()
 app = Flask(__name__)
+app.secret_key = 'secret'
+user = None
+error_msg = None
+@app.route('/login-user', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            session['user'] = user
+            return redirect(url_for('home'))
+        except:
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    if user is not None:
+        return redirect(url_for('home'))
+    return render_template('signup.html')
+
+
+@app.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+    email = request.form['email']
+    password = request.form['password']
+    user = auth.create_user_with_email_and_password(email, password)
+    session['user'] = user
+    print(session)
+    return redirect(url_for('home'))
+@app.route('/login')
+def login():
+    if 'user' in session:
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    if 'user' in session:
+        user = session['user']
+        email = user['email']
+        if 'results' in session:
+            results = session['results']
+            print(results)
+            return render_template('dashboard.html', email=email, results=results, days_of_the_week=days_of_the_week, today_and_next_day=today_and_next_day)
+        return render_template('dashboard.html', user=user, email=email, results = [])
+    return redirect(url_for('login'))
 
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    session.pop('results', None)
+    return redirect(url_for('index'))
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        weight = int(request.form['weight'])
+        calories_want = int(request.form['calories_want'])
+        height = int(request.form['height'])
+        df = pd.read_csv("datasets/exercise_dataset.csv")
+        if weight < 140:
+            loaded_model = joblib.load('130weightModel.sav')
+        elif weight < 170:
+            loaded_model = joblib.load('155weightModel.sav')
+        elif weight < 200:
+            loaded_model = joblib.load('180weightModel.sav')
+        else:
+            loaded_model = joblib.load('205weightModel.sav')
+        results = [age, weight, height, calories_want]
+        for j in range(6):
+            result = round(loaded_model.predict([calories_want]))
+            with open("datasets/exercise_dataset.csv", 'r') as f:
+                for (i, line) in enumerate(range(result+1)):
+                    dfline = f.readline(line)
+                    if i == result:
+                        exercise = dfline.split(",")[0]
+                        exercise_detail = dfline.split(",")[1]
+                        try:                       
+                            if exercise_detail[0] == "4" or exercise_detail[0] == "5" or exercise_detail[0] == "6" or exercise_detail[0] == "7" or exercise_detail[0] == "8" or exercise_detail[0] == "9" or exercise_detail[0] == "0" or exercise_detail[0] == "1" or exercise_detail[0] == "2" or exercise_detail[0] == "3":
+                                exercise_detail = "No specifics for this, just repeatedly use it"
+                        except:
+                            pass
+                        if 'user' in session:
+                            user = session['user']
+                            email = user['email']
+                            print(j)
+                            if (exercise[0] == '"'):
+                                exercise = exercise[1:]
+                            if (exercise[-1] == '"'):
+                                exercise = exercise[:-1]
+                            if (exercise_detail[0] == '"'):
+                                exercise_detail = exercise_detail[1:]
+                            if (exercise_detail[-1] == '"'):
+                                exercise_detail = exercise_detail[:-1]
+                            else:
+                                pass
+                            results.append([exercise, exercise_detail, days_of_the_week[j], today_and_next_day[j]])
+            calories_want -= 100
+        session['results'] = results
+    return redirect(url_for('home'))
+@app.route('/map')
+def map():
+    if 'user' in session:
+        user = session['user']
+        email = user['email']
+        return render_template('fitness.html', email=email)
+    else:
+        return redirect(url_for('login'))
 app.run(host='0.0.0.0', port=81, debug=True)
